b4f23460dc6d83d91203193360fd62f6d994ca22
Paul Hultgren <pjhultgren5@gmail.com>
2022-07-01 21:31:24 -0700

    add gatsby-plugin-netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,6 +44,7 @@ module.exports = {
                 },
             },
         },
+        `gatsby-plugin-netlify`,
         'gatsby-plugin-react-helmet',
         `gatsby-plugin-sass`,
         `gatsby-plugin-typescript`,
